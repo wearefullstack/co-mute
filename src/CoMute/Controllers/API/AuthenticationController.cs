90ff7ae6b26db7cbc7bf672348ea1952299dedd8
Markus Wynand Venter <markuswynandventer@gmail.com>
2023-03-10 15:05:33 +0200

    Added Pages and Controllers for those pages

--- src/CoMute/Controllers/API/AuthenticationController.cs
+++ src/CoMute/Controllers/API/AuthenticationController.cs
@@ -10,11 +10,10 @@ namespace CoMute.Web.Controllers.API
 {
     public class AuthenticationController : ApiController
     {
-        /// <summary>
-        /// Logs a user into the application.
-        /// </summary>
-        /// <param name="loginRequest">The user's login details</param>
-        /// <returns></returns>
+        //Takes the loginRequest data and then check to see if the user exists and if the password exists. 
+        //It returns a OK if the password and Email combo words together. It returns a Not Found if the User doesn't Exist or Password is incorrect.
+        //The Post comes action comes from the "login.js" file in the conent folder under the js folder. The Return response is also sent back there.
+        //The DB Model is found under the Model folder in the DAL folder.
         public HttpResponseMessage Post(LoginRequest loginRequest)
         {
             ComuteDBEntities db = new ComuteDBEntities();
--- src/CoMute/Controllers/Web/SearchController.cs
+++ src/CoMute/Controllers/Web/SearchController.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CoMute.Web.Models.DAL;
 
 namespace CoMute.Web.Controllers.Web
 {
-    public class HomeController : Controller
+    public class SearchController : Controller
     {
+        // GET: Search
         public ActionResult Index()
+        {            
+            return View();
+        }
+
+        public ActionResult CreateCarpool()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            return View();
-        }
-
-        public ActionResult Register()
+        public ActionResult ViewCarpool()
         {
             return View();
         }
