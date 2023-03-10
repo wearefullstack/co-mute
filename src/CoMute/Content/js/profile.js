522609ed2f54805da07227d793c90494606350de
Markus Wynand Venter <markuswynandventer@gmail.com>
2023-03-10 18:06:06 +0200

    Success and Error on success for the profile page

--- src/CoMute/Content/js/profile.js
+++ src/CoMute/Content/js/profile.js
@@ -35,9 +35,28 @@
             var pswd = $('#Password').val();
 
             $.post('/api/profile', { name: name, surname: surname, phoneNumber: phone, emailAddress: email, password: pswd }, function (data) {
+                var $alert = $('#success');
+                var $p = $alert.find("p");
+                $p.text('Profile Update Successful');
+                $alert.removeClass('hidden');
 
+                window.scrollTo(0, 0);
+
+                setTimeout(function () {
+                    $p.text('');
+                    $alert.addClass('hiddne');
+                    location.reload();;
+                }, 3000);
             }).fail(function (data) {
+                var $alert = $("#error");
+                var $p = $alert.find("p");
+                $p.text('Update failed');
+                $alert.removeClass('hidden');
 
+                setTimeout(function () {
+                    $p.text('');
+                    $alert.addClass('hidden');
+                }, 3000);
             });
         }
     });
