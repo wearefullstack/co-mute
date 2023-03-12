87b4d7480a539ac2b535c6376c9a4e128631ab71
Markus Wynand Venter <markuswynandventer@gmail.com>
2023-03-12 16:40:20 +0200

    Added search function to Search Page and Added the names of the oweners

---
+++ src/CoMute/Content/js/search.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('#searchTerm').on('keyup', function () {
+        var value = $(this).val();
+        $('#viewOfCarpool tr').filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
